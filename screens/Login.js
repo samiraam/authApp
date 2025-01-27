b40d1b7e8b53c8af745bdb147c3a4ebf6e73c878
samiraam <mecinajsamira1@gmail.com>
2025-01-27 08:42:47 -0800

    init push

---
+++ screens/Login.js
@@ -0,0 +1,49 @@
+import React, {useState} from "react";
+import {View, Text, TextInput, Button, Stylesheet} from 'react-native'
+
+const Login=()=>{
+    const [username, setUsername]=useState("")
+    const [password, setPassword]=useState("")
+    const Handle=async()=>{
+        try{
+            const pergjigjja= await fetch("https://dummyjson.com/auth/login",{
+                method: "POST",
+                headers:{'Content-Type':"application/json"},
+                body:JSON.stringify({
+                    username:username,
+                    password:password
+                }),
+                credentials:"omit"
+            })
+            const data=await pergjigjja.json()
+            console.log(data)
+
+        }
+catch(err){
+
+    }
+ }
+    return(
+        <View>
+            <Text> Welcome to fake api call</Text>
+            <TextInput
+                placeholder ="Username"
+                autoCapitallize= "none"
+                value= {username}
+                onChangeText= {text=>setUsername(text)}
+            />
+            <TextInput
+                placeholder ="Password"
+                autoCapitallize= "none"
+                secureTextEntry={true}
+                value= {password}
+                onChangeText= {text=>setPassword(text)}
+            />
+            <Button title="Login" onPress={Handle}/>
+
+        </View>
+    )
+
+
+}
+export default Login;
